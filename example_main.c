e8c43c357e564afc55525130a8103334cf54c4d8
mahersanalla <50663775+mahersanalla@users.noreply.github.com>
2020-11-29 22:29:06 +0200

    Delete example_main.c

--- example_main.c
+++
@@ -1,9 +0,0 @@
-#include "QOSInterface/interface.h"
-#include <stdio.h>
-
-int main()
-{
-	setNetDevice("router-server");
-	printf ("The size of the queue is: %d\n", printQueueSize());
-	return 0;
-}
